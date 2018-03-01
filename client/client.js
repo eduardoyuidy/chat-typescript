ca926b4b2079d90088910b43e904a7c811353408
Sapão <andreoliveira1982@gmail.com>
2018-02-28 23:22:59 -0300

    Tentativa de usar cores

--- client/client.js
+++ client/client.js
@@ -5,13 +5,17 @@ var colors = require("colors");
 var readline = require("readline");
 var uuid = require("uuid/v1");
 var model_1 = require("./../model");
+var model_2 = require("./../model");
 var socket = socketIO('http://localhost:3000');
 var readlineStream = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-var userColors = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'gray', 'grey'];
-var currUser = new model_1.UserDetails('', userColors[Math.floor(Math.random() * userColors.length)], uuid());
+function getRandomColor() {
+    var index = Math.floor(Math.random() * Object.keys(model_2.UserColors).length / 2);
+    return model_2.UserColors[index];
+}
+var currUser = new model_1.UserDetails('', getRandomColor(), uuid());
 socket.on('connect', function () {
     readlineStream.question(colors.bgRed('Informe seu nome de usuário: '), function (name) {
         currUser.username = name || 'anônimo';
@@ -26,6 +30,6 @@ socket.on('connect', function () {
 socket.on('broadcast', function (user, message) {
     if (user.id !== currUser.id) {
         var userColor = user.color;
-        console.log(user.username + ': ' + message);
+        console.log(colors[userColor](user.username + ': ' + message));
     }
 });
